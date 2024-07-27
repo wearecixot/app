f7a062b8d81b36e41e6069d76b4750c659ec5417
Abiel Zulio M <7030944+abielzulio@users.noreply.github.com>
2024-07-28 03:49:04 +0700

    update

--- src/app/rewards/page.tsx
+++ src/app/rewards/page.tsx
@@ -1,39 +1,38 @@
-'use client';
+"use client"
 
-import Layout from "@/components/Layout";
-import { useState } from "react";
+import Layout from "@/components/Layout"
+import { useState } from "react"
 import axios from "@/libs/axios"
 
-import { User, ChevronLeft, ChevronRight } from "lucide-react";
-import { useQuery } from "@tanstack/react-query";
-import { REWARDS_TIER_ENUM } from "@/constants/rewards";
-import { cn } from "@/utils/cn";
+import { User, ChevronLeft, ChevronRight } from "lucide-react"
+import { useQuery } from "@tanstack/react-query"
+import { REWARDS_TIER_ENUM } from "@/constants/rewards"
+import { cn } from "@/utils/cn"
+import { TIER_REWARDS } from "@/constants/mocks"
 
 const Rewards = () => {
-  const [tier, setTier] = useState<number>(0);
+  const [tier, setTier] = useState<number>(0)
 
   const previousTier = () => {
-    if (tier === 0) return;
-    setTier(t => t - 1);
+    if (tier === 0) return
+    setTier((t) => t - 1)
   }
 
   const nextTier = () => {
-    if (tier === 3) return;
-    setTier(t => t + 1);
+    if (tier === 3) return
+    setTier((t) => t + 1)
   }
 
   const { data: rewards } = useQuery({
     queryKey: ["rewards"],
     queryFn: () => axios.get("/reward/list"),
-  });
+  })
 
   return (
     <Layout className="flex flex-col p-0">
       <div className="w-full flex items-center justify-between bg-tertiary py-6 px-4">
         <ChevronLeft
-          className={cn(
-            tier === 0 ? "opacity-0" : "cursor-pointer"
-          )}
+          className={cn(tier === 0 ? "opacity-0" : "cursor-pointer")}
           onClick={previousTier}
         />
         <div className="flex flex-col items-center gap-2">
@@ -41,19 +40,23 @@ const Rewards = () => {
           <p>Tier {tier + 1}</p>
         </div>
         <ChevronRight
-          className={cn(
-            tier === 2 ? "opacity-0" : "cursor-pointer"
-          )}
+          className={cn(tier === 2 ? "opacity-0" : "cursor-pointer")}
           onClick={nextTier}
         />
       </div>
       <div className="w-full flex flex-col gap-4 p-4 flex-1 overflow-y-auto">
-        {!!rewards && rewards.data.data.filter((r: any) => r.tier === REWARDS_TIER_ENUM[tier + 1]).map((reward: any, index: number) => (
-          <p key={index}>{reward.name}</p>
-        ))}
+        {/*         {TIER_REWARDS[tier].map((reward: any, index: number) => (
+          <p key={index}>{reward}</p>
+        ))} */}
+        {!!rewards &&
+          rewards.data.data
+            .filter((r: any) => r.tier === REWARDS_TIER_ENUM[tier + 1])
+            .map((reward: any, index: number) => (
+              <p key={index}>{reward.name}</p>
+            ))}
       </div>
     </Layout>
-  );
-};
+  )
+}
 
-export default Rewards;
+export default Rewards
