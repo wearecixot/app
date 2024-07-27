2c85ece849761115446f52ddb55876e0842a3284
Abiel Zulio M <7030944+abielzulio@users.noreply.github.com>
2024-07-28 01:46:43 +0700

    update

--- src/components/RewardList/index.tsx
+++ src/components/RewardList/index.tsx
@@ -3,13 +3,15 @@ import RewardCard from "./components/RewardCard"
 
 const RewardList: FC = () => {
   return (
-    <section className="flex flex-col gap">
+    <section className="flex flex-col gap relative">
+      <div className="h-full w-[15px] bg-gradient-to-r from-white to-transparent absolute top-0 left-0 z-10" />
       <p className="font-semibold text-xl px-4">Rewards</p>
       <div className="flex gap-4 overflow-x-scroll p-4">
         <RewardCard />
         <RewardCard />
         <RewardCard />
       </div>
+      <div className="h-full w-[15px] bg-gradient-to-l from-white to-transparent  absolute top-0 right-0 z-10" />
     </section>
   )
 }
