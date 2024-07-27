c9052da179ea1fc6f6f7fb2372dda1fa5ce69f75
Vincent Suryakim <vincentsuryakim@gmail.com>
2024-07-27 14:59:44 +0700

    Initial commit

---
+++ src/app/layout.tsx
@@ -0,0 +1,26 @@
+import type { Metadata } from "next";
+import { GeistSans } from 'geist/font/sans';
+import "./globals.css";
+import '@radix-ui/themes/styles.css';
+import { Theme } from '@radix-ui/themes';
+
+export const metadata: Metadata = {
+  title: "Hidup Sehat",
+  description: "Hidup Sehat Bahagia",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en" className={GeistSans.className}>
+      <body className="max-w-[450px] mx-auto shadow-xl">
+        <Theme>
+          {children}
+        </Theme>
+      </body>
+    </html>
+  );
+}
---
+++ src/app/page.tsx
@@ -0,0 +1,5 @@
+const Home = () => {
+  return <p className="text-3xl font-semibold">Home</p>;
+};
+
+export default Home;
