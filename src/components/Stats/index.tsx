16faa7b29ccba3ebcd50b173fc48886acc497d9e
Abiel Zulio M <7030944+abielzulio@users.noreply.github.com>
2024-07-27 22:34:14 +0700

    stats

--- src/components/Stats/index.tsx
+++ src/components/Stats/index.tsx
@@ -12,124 +12,85 @@ import { ArrowRight } from "lucide-react"
 
 const Stats: FC = () => {
   return (
-    <section className="flex flex-col gap-2 w-full p-3 rounded-lg border-[1px] border-green-500/10 bg-green-500/5">
-      <div className="flex items-center justify-between">
-        <div className="flex flex-col gap-1">
-          <p className="text-md font-medium opacity-50 text-green-800">
-            Redeemable points
-          </p>
-          <div className="flex items-start gap-2">
-            <Coins size={24} className="mt-2 text-green-600 fill-green-200" />
+    <section className="flex flex-col w-full rounded-lg border-[1px] border-green-800/10 bg-white drop-shadow-xl">
+      <div className="flex flex-col gap-4 p-4 rounded-b-lg">
+        <div className="flex items-center justify-between">
+          <div className="flex flex-col gap-1">
+            <p className="text-sm font-medium text-green-900/50">
+              Redeemable points
+            </p>
+            <div className="flex items-start gap-2">
+              <Coins size={24} className="mt-2 text-green-600 fill-green-200" />
 
-            <p className="text-3xl font-bold text-green-500">100</p>
+              <p className="text-3xl font-bold text-green-500">100</p>
+            </div>
           </div>
+          <Button
+            variant="plain"
+            size="lg"
+            className="bg-green-500 hover:bg-green-600 text-white"
+          >
+            <Gift size={24} className="mr-2" />
+            Open Gift
+          </Button>
         </div>
-        <Button
-          variant="plain"
-          size="lg"
-          className="bg-green-500 hover:bg-green-600 text-white"
-        >
-          <Gift size={24} className="mr-2" />
-          Open Gift
-        </Button>
-      </div>
-      <span className="h-[2px] bg-green-500/10 w-full rounded-md" />
-      <div className="grid grid-cols-3">
-        <div className="flex flex-col gap-1">
-          <p className="text-sm font-medium opacity-50 text-green-800">
-            Total calories
-          </p>
-          <div className="flex items-start gap-1">
-            <Flame
-              fill
-              size={16}
-              className="mt-1.5 text-green-600 fill-green-100"
-            />
-            <p className="text-lg font-semibold text-green-500">100 kcal</p>
+        <div className="grid grid-cols-3">
+          <div className="flex flex-col gap-1">
+            <p className="text-sm font-medium text-green-900/50">
+              Total calories
+            </p>
+            <div className="flex items-start gap-1">
+              <Flame
+                fill
+                size={16}
+                className="mt-1.5 text-green-600 fill-green-100"
+              />
+              <p className="text-lg font-semibold text-green-500">100 kcal</p>
+            </div>
           </div>
-        </div>
-        <div className="flex flex-col gap-1">
-          <p className="text-sm font-medium opacity-50 text-green-800">
-            Total distance
-          </p>
-          <div className="flex items-start gap-1">
-            <GitCommitVertical
-              size={16}
-              fill
-              className="mt-1.5 text-green-600 fill-green-100"
-            />
-            <p className="text-lg font-semibold text-green-500">100 km</p>
+          <div className="flex flex-col gap-1">
+            <p className="text-sm font-medium text-green-900/50">
+              Total distance
+            </p>
+            <div className="flex items-start gap-1">
+              <GitCommitVertical
+                size={16}
+                fill
+                className="mt-1.5 text-green-600 fill-green-100"
+              />
+              <p className="text-lg font-semibold text-green-500">100 km</p>
+            </div>
           </div>
-        </div>
-        <div className="flex flex-col gap-1">
-          <p className="text-sm font-medium opacity-50 text-green-800">
-            Total time
-          </p>
-          <div className="flex items-start gap-1">
-            <Timer
-              fill
-              size={16}
-              className="mt-1.5 text-green-600 fill-green-100"
-            />
-            <p className="text-lg font-semibold text-green-500">100 second</p>
+          <div className="flex flex-col gap-1">
+            <p className="text-sm font-medium text-green-900/50">Total time</p>
+            <div className="flex items-start gap-1">
+              <Timer
+                fill
+                size={16}
+                className="mt-1.5 text-green-600 fill-green-100"
+              />
+              <p className="text-lg font-semibold text-green-500">100 second</p>
+            </div>
           </div>
         </div>
       </div>
-      <span className="h-[2px] bg-green-500/10 w-full rounded-md" />
-      <Link className="flex items-start justify-between gap-3" href="/level">
+
+      <Link
+        className="flex items-start border-t-[1px] border-green-800/10 justify-between gap-3 p-4 bg-green-800/5 hover:bg-green-900/5 transition duration-200 ease-in-out"
+        href="/level"
+      >
         <div className="relative flex items-center justify-center text-center">
           <Badge size={36} className=" text-green-600 fill-green-200" />
-          <p className="absolute z-10 text-md m-auto text-xs text-green-800">
+          <p className="absolute z-10 text-md m-auto text-xs text-green-600">
             1
           </p>
         </div>
         <div className="flex flex-col gap-2 w-full">
-          <p className="text-sm font-medium opacity-50 text-green-800">Level</p>
+          <p className="text-sm font-medium text-green-900/50">Level</p>
           <Progress value={12} className="w-full" />
         </div>
-        <ArrowRight className="my-auto text-green-500" />
+        <ArrowRight className="my-auto text-green-900/50" />
       </Link>
-    </section>
-  )
-  return (
-    <section className="flex flex-col gap-3 w-full">
-      <div className="flex items-start justify-between">
-        <div className="flex flex-col gap-1">
-          <p className="text-sm font-medium opacity-80 text-black/50">
-            Redeemable points
-          </p>
-          <div className="flex items-center gap-2 text-green-600 fill-green-200">
-            <Coins fill />
-            <p className="text-5xl font-bold">100</p>
-          </div>
-        </div>
-        <div className="flex flex-col items-end">
-          <p className="text-sm font-medium opacity-80 text-black/50">
-            Total calories burned
-          </p>
-          <div className="flex items-center gap-2 text-green-600 fill-green-200">
-            <Flame size={14} />
-            <p className="text-2xl font-bold">100</p>
-          </div>
-        </div>
-      </div>
-      {/*       <Button
-        variant="plain"
-        size="lg"
-        className="bg-green-500 hover:bg-green-600 text-white"
-      >
-        Redeem
-      </Button> */}
-    </section>
-  )
-  return (
-    <section className="flex bg-secondary items-center w-full rounded-lg p-4">
-      <div className="flex flex-1 flex-col">
-        <p className="text-sm">Your Points</p>
-        <p className="text-3xl font-bold">100</p>
-        <p className="text-xs mt-2">You have burned 350 calories 🔥</p>
-      </div>
-      <Button>Redeem</Button>
     </section>
   )
 }
