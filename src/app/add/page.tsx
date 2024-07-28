0612e50d3beed553cc8513aac16fb71d1906a904
Vincent Suryakim <vincentsuryakim@gmail.com>
2024-07-28 08:27:34 +0700

    feat: integrate commute

--- src/app/add/page.tsx
+++ src/app/add/page.tsx
@@ -5,23 +5,56 @@ import { Input } from "@/components/input"
 import Layout from "@/components/Layout"
 import TabsList from "@/components/TabsList"
 import Tab from "@/components/TabsList/components/Tab"
+import { useRewardsContext } from "@/contexts/RewardsContext"
+import axios from "@/libs/axios"
 import { cn } from "@/utils/cn"
+import { useMutation } from "@tanstack/react-query"
 import { RefreshCcw } from "lucide-react"
 import { Coins } from "lucide-react"
 import { TrainFront } from "lucide-react"
 import { Bike } from "lucide-react"
 import { CreditCard } from "lucide-react"
+import { useRouter } from "next/navigation"
 import React from "react"
 
 type Tab = "run" | "bike" | "commute"
 
 const Commute = () => {
   const [isLoaded, setIsLoaded] = React.useState(false)
+  const [points, setPoints] = React.useState<number>(0);
+  const [inStation, setInStation] = React.useState<string>("");
+  const [outStation, setOutStation] = React.useState<string>("");
+
+  const router = useRouter();
+
+  const { setShowConfetti } = useRewardsContext()
+
+  const { mutate } = useMutation({
+    mutationKey: ["add-commute"],
+    mutationFn: () => {
+      return axios.post("/activities/add/transport")
+    },
+    onSuccess: (res) => {
+      const data = res?.data.data
+      setPoints(data.pointAmount)
+      setInStation(data.in)
+      setOutStation(data.out)
+      setIsLoaded(true)
+      setShowConfetti(true)
+    },
+  })
+
   return (
-    <button
-      onClick={() => setIsLoaded((prev) => !prev)}
+    <div
+      onClick={() => {
+        if (isLoaded) {
+          router.push('/')
+        } else {
+          mutate()
+        }
+      }}
       className={cn(
-        "w-full h-full p-4 text-center rounded-md bg-gray-50 border-[1px] flex flex-col gap-2 justify-center items-center",
+        "cursor-pointer w-full h-full p-4 text-center rounded-md bg-gray-50 border-[1px] flex flex-col gap-2 justify-center items-center",
         {
           "bg-gray-50": !isLoaded,
           "bg-green-100/20 border-green-200/50": isLoaded,
@@ -35,7 +68,7 @@ const Commute = () => {
             Please tap your electronic money
           </p>
           <p className="opacity-50 text-sm">
-            We'll automatically get your last public transporation transaction
+            We&apos;ll automatically get your last public transporation transaction
           </p>
         </React.Fragment>
       ) : (
@@ -45,10 +78,11 @@ const Commute = () => {
           </div>
 
           <p className="font-semibold text-green-500">
-            You got 90 points for your last transaction
+            Congratulations!
           </p>
-          <p className="opacity-50 text-sm text-green-500">
-            We'll automatically get your last public transporation transaction
+          <p className="opacity-80 text-sm text-green-500">
+            You got {points} points for your last commute from {inStation} to {outStation}
+            {/* We&apos;ll automatically get your last public transporation transaction */}
           </p>
           <Button
             variant="plain"
@@ -60,7 +94,7 @@ const Commute = () => {
           </Button>
         </React.Fragment>
       )}
-    </button>
+    </div>
   )
 }
 
