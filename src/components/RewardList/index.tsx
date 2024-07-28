ae6388f4d1eed01bbc81dec925b0ee5ce277a404
Abiel Zulio M <7030944+abielzulio@users.noreply.github.com>
2024-07-28 10:34:17 +0700

    skeleton

--- src/components/RewardList/index.tsx
+++ src/components/RewardList/index.tsx
@@ -1,4 +1,4 @@
-'use client'
+"use client"
 
 import React, { FC } from "react"
 import RewardCard from "./components/RewardCard"
@@ -11,7 +11,30 @@ const RewardList: FC = () => {
   return (
     <section className="flex flex-col gap relative">
       <p className="font-semibold text-xl px-4">Rewards</p>
-      {myRewardsData?.data.data.length > 0 ? (
+      {isMyRewardsLoading ? (
+        <div className="flex gap-4 p-4">
+          <div className="shadow-md h-[200px] min-w-[200px] overflow-hidden flex flex-col rounded-md drop-shadow-md bg-white">
+            <div className="text-center overflow-hidden h-[150px] w-full aspect-square relative bg-gray-200 animate-pulse" />
+            <div className="flex flex-col h-full gap-1 p-2.5">
+              <div className="w-[80px] h-5 bg-black/10 animate-pulse rounded-md" />
+              <div className="w-[80px] h-5 bg-black/10 animate-pulse rounded-md" />
+              {true ? (
+                <div className="w-full h-8 mt-auto bg-black/10 animate-pulse rounded-md" />
+              ) : null}
+            </div>
+          </div>
+          <div className="shadow-md h-[200px] min-w-[200px] overflow-hidden flex flex-col rounded-md drop-shadow-md bg-white">
+            <div className="text-center overflow-hidden h-[150px] w-full aspect-square relative bg-gray-200 animate-pulse" />
+            <div className="flex flex-col h-full gap-1 p-2.5">
+              <div className="w-[80px] h-5 bg-black/10 animate-pulse rounded-md" />
+              <div className="w-[80px] h-5 bg-black/10 animate-pulse rounded-md" />
+              {true ? (
+                <div className="w-full h-8 mt-auto bg-black/10 animate-pulse rounded-md" />
+              ) : null}
+            </div>
+          </div>
+        </div>
+      ) : myRewardsData?.data.data.length > 0 ? (
         <React.Fragment>
           <div className="h-full w-[15px] bg-gradient-to-r from-white to-transparent absolute top-0 left-0 z-10" />
           <div className="flex gap-4 overflow-x-scroll p-4">
