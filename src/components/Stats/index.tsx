98792aafb5cdb2e0d39eb5a1f1b6533905c3a923
Abiel Zulio M <7030944+abielzulio@users.noreply.github.com>
2024-07-28 10:23:58 +0700

    skeleton

--- src/components/Stats/index.tsx
+++ src/components/Stats/index.tsx
@@ -20,23 +20,27 @@ import { Progress } from "../progress"
 import { useRouter } from "next/navigation"
 
 const Stats: FC = () => {
-  const { mutateRedeemRewards, isMutateRewardsPending, activityHeadersData } =
-    useRewardsContext()
-  
   const router = useRouter()
+
+  const {
+    mutateRedeemRewards,
+    isMutateRewardsPending,
+    isActivityHeadersLoading,
+    activityHeadersData,
+  } = useRewardsContext()
 
   const tier =
     REWARDS_TIER_DICT[
       activityHeadersData?.data?.data?.tier as keyof typeof REWARDS_TIER_DICT
     ]
-  
-    const handleOpenGift = () => {
-      if ((activityHeadersData?.data?.data?.balance ?? 0) >= 100) {
-        mutateRedeemRewards()
-      } else {
-        router.push("stravaride://")
-      }
+
+  const handleOpenGift = () => {
+    if ((activityHeadersData?.data?.data?.balance ?? 0) >= 100) {
+      mutateRedeemRewards()
+    } else {
+      router.push("stravaride://")
     }
+  }
 
   return (
     <div className="px-4">
@@ -58,11 +62,15 @@ const Stats: FC = () => {
                   className="mt-2 text-green-600 fill-green-200"
                 />
 
-                <p className="text-4xl font-bold text-green-500">
-                  {(
-                    activityHeadersData?.data?.data?.balance ?? 0
-                  ).toLocaleString()}
-                </p>
+                {isActivityHeadersLoading ? (
+                  <div className="bg-green-800/10 w-[100px] h-10 rounded-md animate-pulse" />
+                ) : (
+                  <p className="text-4xl font-bold text-green-500">
+                    {(
+                      activityHeadersData?.data?.data?.balance ?? 0
+                    ).toLocaleString()}
+                  </p>
+                )}
               </div>
             </div>
             <div className="flex flex-col gap-1">
@@ -75,12 +83,16 @@ const Stats: FC = () => {
                   size={16}
                   className="mt-1.5 text-green-600 fill-green-100"
                 />
-                <p className="text-lg font-semibold text-green-500">
-                  {(
-                    activityHeadersData?.data?.data?.totalCalories ?? 0
-                  ).toLocaleString()}{" "}
-                  kcal
-                </p>
+                {isActivityHeadersLoading ? (
+                  <div className="bg-green-800/10 w-[70px] h-6 rounded-md animate-pulse" />
+                ) : (
+                  <p className="text-lg font-semibold text-green-500">
+                    {(
+                      activityHeadersData?.data?.data?.totalCalories ?? 0
+                    ).toLocaleString()}{" "}
+                    kcal
+                  </p>
+                )}
               </div>
             </div>
           </div>
@@ -95,11 +107,15 @@ const Stats: FC = () => {
                   fill
                   className="mt-1.5 text-green-600 fill-green-100"
                 />
-                <p className="text-lg font-semibold text-green-500">
-                  {(
-                    activityHeadersData?.data?.data?.totalRun ?? 0
-                  ).toLocaleString()}
-                </p>
+                {isActivityHeadersLoading ? (
+                  <div className="bg-green-800/10 w-[60px] h-6 rounded-md animate-pulse" />
+                ) : (
+                  <p className="text-lg font-semibold text-green-500">
+                    {(
+                      activityHeadersData?.data?.data?.totalRun ?? 0
+                    ).toLocaleString()}
+                  </p>
+                )}
               </div>
             </div>
 
@@ -113,11 +129,15 @@ const Stats: FC = () => {
                   fill
                   className="mt-1.5 text-green-600 fill-green-100"
                 />
-                <p className="text-lg font-semibold text-green-500">
-                  {(
-                    activityHeadersData?.data?.data?.totalBicycle ?? 0
-                  ).toLocaleString()}
-                </p>
+                {isActivityHeadersLoading ? (
+                  <div className="bg-green-800/10 w-[60px] h-6 rounded-md animate-pulse" />
+                ) : (
+                  <p className="text-lg font-semibold text-green-500">
+                    {(
+                      activityHeadersData?.data?.data?.totalBicycle ?? 0
+                    ).toLocaleString()}
+                  </p>
+                )}
               </div>
             </div>
             <div className="flex flex-col gap-1">
@@ -130,27 +150,44 @@ const Stats: FC = () => {
                   fill
                   className="mt-1.5 text-green-600 fill-green-100"
                 />
-                <p className="text-lg font-semibold text-green-500">
-                  {(
-                    activityHeadersData?.data?.data?.totalCommute ?? 0
-                  ).toLocaleString()}
-                </p>
+                {isActivityHeadersLoading ? (
+                  <div className="bg-green-800/10 w-[60px] h-6 rounded-md animate-pulse" />
+                ) : (
+                  <p className="text-lg font-semibold text-green-500">
+                    {(
+                      activityHeadersData?.data?.data?.totalCommute ?? 0
+                    ).toLocaleString()}
+                  </p>
+                )}
               </div>
             </div>
           </div>
 
-          <Button
-            variant="plain"
-            size="lg"
-            className="bg-green-500 hover:bg-green-600 w-full text-white"
-            onClick={handleOpenGift}
-            isLoading={isMutateRewardsPending}
-          >
-            <Gift size={20} className="mr-2" />
-            {(activityHeadersData?.data?.data?.balance ?? 0) >= 100
-              ? "Open Gift"
-              : "Open Strava"}
-          </Button>
+          {isActivityHeadersLoading ? (
+            <div className="h-11 w-full rounded-md bg-green-800/10 animate-pulse" />
+          ) : (
+            <Button
+              variant="plain"
+              size="lg"
+              className={cn("w-full text-white", {
+                "bg-green-500 hover:bg-green-600":
+                  !isActivityHeadersLoading &&
+                  (activityHeadersData?.data?.data?.balance ?? 0) >= 100,
+                "bg-orange-500 hover:bg-orange-600":
+                  !isActivityHeadersLoading &&
+                  (activityHeadersData?.data?.data?.balance ?? 0) < 100,
+              })}
+              onClick={handleOpenGift}
+              isLoading={isMutateRewardsPending}
+            >
+              {(activityHeadersData?.data?.data?.balance ?? 0) >= 100 ? (
+                <Gift size={20} className="mr-2" />
+              ) : null}
+              {(activityHeadersData?.data?.data?.balance ?? 0) >= 100
+                ? "Open Gift"
+                : "Open Strava"}
+            </Button>
+          )}
         </div>
 
         <Link
@@ -160,36 +197,46 @@ const Stats: FC = () => {
           <div className="relative flex items-center justify-center text-center">
             <Badge
               size={36}
-              className={cn(" drop-shadow-md", {
-                "text-amber-700 fill-amber-300": tier === 1 || tier === 3,
-                "text-gray-700 fill-gray-300": tier === 2,
+              className={cn("drop-shadow-md", {
+                "text-amber-700 fill-amber-300":
+                  !isActivityHeadersLoading && (tier === 1 || tier === 3),
+                "text-gray-700 fill-gray-300":
+                  !isActivityHeadersLoading && tier === 2,
+                "text-black fill-white animate-pulse opacity-50":
+                  isActivityHeadersLoading,
               })}
             />
-            <p
-              className={cn(
-                "absolute z-10 text-md m-auto text-xs text-green-600",
-                {
-                  "text-amber-600": tier === 1 || tier === 3,
-                  "text-gray-700": tier === 2,
-                }
-              )}
-            >
-              {tier}
-            </p>
+            {isActivityHeadersLoading ? null : (
+              <p
+                className={cn(
+                  "absolute z-10 text-md m-auto text-xs text-green-600",
+                  {
+                    "text-amber-600": tier === 1 || tier === 3,
+                    "text-gray-700": tier === 2,
+                  }
+                )}
+              >
+                {tier}
+              </p>
+            )}
           </div>
           <div className="flex flex-col gap-2 w-full">
             <p className="text-sm font-medium text-black/50">Level</p>
-            <Progress
-              classNames={{
-                root: tier === 2 ? "bg-gray-700/20" : "bg-amber-700/20",
-                indicator: tier === 2 ? "bg-gray-700" : "bg-amber-700",
-              }}
-              value={Math.min(
-                100,
-                (activityHeadersData?.data?.data?.tierProgress ?? 0) * 10
-              )}
-              className="w-full"
-            />
+            {isActivityHeadersLoading ? (
+              <div className="w-full h-[7px] bg-black/10 rounded-md animate-pulse" />
+            ) : (
+              <Progress
+                classNames={{
+                  root: tier === 2 ? "bg-gray-700/20" : "bg-amber-700/20",
+                  indicator: tier === 2 ? "bg-gray-700" : "bg-amber-700",
+                }}
+                value={Math.min(
+                  100,
+                  (activityHeadersData?.data?.data?.tierProgress ?? 0) * 10
+                )}
+                className="w-full"
+              />
+            )}
           </div>
           <ArrowRight className={cn("my-auto text-black/50")} />
         </Link>
