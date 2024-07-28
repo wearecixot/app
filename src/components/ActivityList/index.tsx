899d11e62f91f47d4e81dfd64bafa05bb357619f
Abiel Zulio M <7030944+abielzulio@users.noreply.github.com>
2024-07-28 10:00:45 +0700

    skelton

--- src/components/ActivityList/index.tsx
+++ src/components/ActivityList/index.tsx
@@ -1,13 +1,10 @@
 "use client"
 
-import { FC } from "react"
-import ActivityCard from "./components/ActivityCard"
-import { MOCK_DATA } from "@/constants/mocks"
-import { Ticket } from "lucide-react"
-import { Bike } from "lucide-react"
-import { Button } from "../button"
-import { RefreshCcw } from "lucide-react"
 import { useRewardsContext } from "@/contexts/RewardsContext"
+import { Bike, RefreshCcw } from "lucide-react"
+import { FC } from "react"
+import { Button } from "../button"
+import ActivityCard from "./components/ActivityCard"
 
 const ActivityList: FC = () => {
   const { activitiesListData, isActivitiesListLoading } = useRewardsContext()
@@ -17,10 +14,39 @@ const ActivityList: FC = () => {
       <div className="flex items-center justify-between w-full">
         <p className="font-semibold text-xl">Activities</p>
 
-        <RefreshCcw size={14} />
+        {!!activitiesListData?.data.data ? <RefreshCcw size={14} /> : null}
       </div>
 
-      {!!activitiesListData?.data.data ? (
+      {isActivitiesListLoading ? (
+        <div className="flex flex-col gap-4">
+          <section className="flex items-start w-full gap-3">
+            <div className="w-10 h-10 animate-pulse bg-black/10 rounded-full flex items-center justify-center relative" />
+            <div className="flex flex-col gap-2 h-full w-[85%]">
+              <div className="flex items-center justify-between w-full">
+                <div className="w-[100px] h-2 bg-black/10 animate-pulse" />
+                <div className="w-[60px] h-2 bg-black/10 animate-pulse" />
+              </div>
+              <div className="flex items-center w-full justify-between">
+                <div className="w-[70px] h-5 bg-black/10 animate-pulse" />
+                <div className="w-[70px] h-5 bg-black/10 animate-pulse" />
+              </div>
+            </div>
+          </section>
+          <section className="flex items-start w-full gap-3">
+            <div className="w-10 h-10 animate-pulse bg-black/10 rounded-full flex items-center justify-center relative" />
+            <div className="flex flex-col gap-2 h-full w-[85%]">
+              <div className="flex items-center justify-between w-full">
+                <div className="w-[100px] h-2 bg-black/10 animate-pulse" />
+                <div className="w-[60px] h-2 bg-black/10 animate-pulse" />
+              </div>
+              <div className="flex items-center w-full justify-between">
+                <div className="w-[70px] h-5 bg-black/10 animate-pulse" />
+                <div className="w-[70px] h-5 bg-black/10 animate-pulse" />
+              </div>
+            </div>
+          </section>
+        </div>
+      ) : !!activitiesListData?.data.data ? (
         <div className="flex flex-col gap-4">
           {activitiesListData?.data.data.map((activity: any) => (
             <ActivityCard
@@ -31,12 +57,19 @@ const ActivityList: FC = () => {
               created_at={activity.createdAt}
               amount={activity.pointAmount}
               is_claimed={activity.isPointClaimed}
-              
               metadata={{
-                ...(!!activity.activityData?.in && { in: activity.activityData.in }),
-                ...(!!activity.activityData?.out && { out: activity.activityData.out }),
-                ...(!!activity.activityData?.calories && { calories: activity.activityData.calories }),
-                ...(!!activity.activityData?.distance && { distance: activity.activityData.distance }),
+                ...(!!activity.activityData?.in && {
+                  in: activity.activityData.in,
+                }),
+                ...(!!activity.activityData?.out && {
+                  out: activity.activityData.out,
+                }),
+                ...(!!activity.activityData?.calories && {
+                  calories: activity.activityData.calories,
+                }),
+                ...(!!activity.activityData?.distance && {
+                  distance: activity.activityData.distance,
+                }),
                 type: activity.activity,
                 merchant: activity.activityData?.merchant ?? "",
               }}
@@ -63,7 +96,6 @@ const ActivityList: FC = () => {
           </div>
         </div>
       )}
-            
     </section>
   )
 }
